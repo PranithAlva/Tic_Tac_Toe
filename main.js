9ca6fd102a79cb77c72b2edb5d3456b503704e14
Pranith P Alva <alvapranith@gmail.com>
2023-08-12 17:31:32 +0530

    Laid the structure for basic desgin using HTML and CSS
        The basic desgin of the project has been completed.
        Used minmal HTML and CSS to overcome problems of
        good desgin. Kept the UI desgin a bit attractive.
        And laid the general structure for how the file
        webpage has to look.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("hello world");
+
+function test() {
+	this.textContent = "x";
+	console.log("hello world");
+}
+const tabs = document.querySelectorAll(".tabs");
+console.log(tabs);
+
+tabs.forEach((tab) => {
+	addEventListener("click", test);
+});
+
+const grid = document.querySelectorAll(".grid");
+grid.forEach((grid) => grid.addEventListener("click", test));
