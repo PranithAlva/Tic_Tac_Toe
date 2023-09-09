bc73f57272685e54bef2c64da1ebcbf72447af10
Pranith P Alva <alvapranith@gmail.com>
2023-09-09 19:28:39 +0530

    Minor changes and full functional dark mode
    The functionality is slowly being shifted to javascript
    with dark mode toggle already being implemented in javascript
    and part of image pannel is slowly being moved to the javascript
    side. Basic things have been done. Change to code complexity will be
    done soon

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-console.log("hello world");
+let mode = true;
+const root = document.documentElement;
+function changeMode() {
+	//direction and enable the corrosponding animation
+	//variable to see in which mode
+
+	if (mode == true) {
+		this.style.animation = "slideX 200ms linear 0ms 1 normal forwards";
+		root.classList.toggle("dark-theme");
+	}
+	if (mode == false) {
+		this.style.animation = "slideY 200ms linear 0ms 1 normal forwards";
+
+		root.classList.toggle("dark-theme");
+	}
+	mode = !mode;
+}
+
+function expand() {
+	this.classList.add("image-pannel-expand");
+	this.classList.remove("image-pannel");
+}
+
+function startGame() {}
+
+function selectDifficulty() {}
+
+const radio = document.getElementById("radio");
+radio.addEventListener("click", changeMode);
+
+const imagePannel = document.querySelectorAll(".image-pannel");
+imagePannel.forEach((pannels) => {
+	pannels.addEventListener("click", expand);
+});
 
 function test() {
 	this.textContent = "X";
