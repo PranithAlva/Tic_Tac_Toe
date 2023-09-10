78f7bf40cac583c3e3721aab2b94c9bb4008e6f4
Pranith P Alva <alvapranith@gmail.com>
2023-09-10 20:36:55 +0530

    Improved slider button and other functionalities using javascript     Now the dark mode slider is better and works more accurately     pricesly than previous iteration. Functionalities of the image     pannel has fully been realised and the logo uses the cloneNode(true)     to get user preference for the user perffered icon. And various small     tweaks are done

--- main.js
+++ main.js
@@ -1,24 +1,59 @@
 let mode = true;
 const root = document.documentElement;
-function changeMode() {
-	//direction and enable the corrosponding animation
-	//variable to see in which mode
+const imagePannel = document.querySelectorAll(".image-pannel");
 
-	if (mode == true) {
-		this.style.animation = "slideX 200ms linear 0ms 1 normal forwards";
-		root.classList.toggle("dark-theme");
-	}
-	if (mode == false) {
-		this.style.animation = "slideY 200ms linear 0ms 1 normal forwards";
+function changeMode(e) {
+	let slideBox = document.getElementById("slide-box");
+	let coordinates = slideBox.getBoundingClientRect();
+	let slideCoordinates = e.clientX;
+	if (coordinates.top <= e.clientY && coordinates.bottom >= e.clientY) {
+		let x =
+			coordinates.left <= slideCoordinates &&
+			(coordinates.right >= slideCoordinates ||
+				coordinates.right <= slideCoordinates);
+		let y =
+			coordinates.right >= slideCoordinates &&
+			(coordinates.left >= slideCoordinates ||
+				coordinates.left <= slideCoordinates);
+		if (x || y) {
+			if (mode == true) {
+				this.style.animation = "slideX 200ms linear 0ms 1 normal forwards";
+				root.classList.toggle("dark-theme");
+			}
 
-		root.classList.toggle("dark-theme");
+			if (mode == false) {
+				this.style.animation = "slideY 200ms linear 0ms 1 normal forwards";
+				root.classList.toggle("dark-theme");
+			}
+			mode = !mode;
+		}
 	}
-	mode = !mode;
 }
 
 function expand() {
-	this.classList.add("image-pannel-expand");
-	this.classList.remove("image-pannel");
+	this.classList.toggle("image-pannel-expand");
+	this.classList.toggle("image-pannel");
+}
+
+function collapse(e) {
+	e.stopPropagation();
+	let parent = this.parentNode.parentNode;
+	parent.classList.toggle("image-pannel");
+	parent.classList.toggle("image-pannel-expand");
+
+	if (parent.dataset.value == "0") {
+		let logo = document.getElementById("left-logo");
+		while (logo.firstChild) logo.removeChild(logo.lastChild);
+		let newLogo = this.cloneNode(true);
+		newLogo.classList.remove("player-marker");
+		logo.appendChild(newLogo);
+	} else {
+		let logo = document.getElementById("right-logo");
+		while (logo.firstChild) logo.removeChild(logo.lastChild);
+		let newLogo = this.cloneNode(true);
+		newLogo.classList.remove("player-marker");
+		logo.appendChild(newLogo);
+	}
 }
 
 function startGame() {}
@@ -27,10 +62,17 @@ function selectDifficulty() {}
 
 const radio = document.getElementById("radio");
 radio.addEventListener("click", changeMode);
+radio.addEventListener("dragstart", () =>
+	radio.addEventListener("dragend", changeMode)
+);
 
-const imagePannel = document.querySelectorAll(".image-pannel");
 imagePannel.forEach((pannels) => {
 	pannels.addEventListener("click", expand);
+});
+
+const playerMarker = document.querySelectorAll(".player-marker");
+playerMarker.forEach((playerMaker) => {
+	playerMaker.addEventListener("click", collapse);
 });
 
 function test() {
